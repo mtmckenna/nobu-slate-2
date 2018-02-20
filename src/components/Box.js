a5b0d8366826339cb6822370272c4165e0db9b10
Matt McKenna <matt@mtmckenna.com>
2018-02-20 07:58:22 -0800

    Can swipe up/down to change scene/take value

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import GestureRecognizer from 'react-native-swipe-gestures';
 
 export default class Box extends Component {
+
+  get value() { return this.state.value || 1; }
+
+  constructor(props) {
+    super(props);
+    this.state = { value : 1 };
+  }
+
+  onSwipeUp() {
+    const nextVal = this.value + 1;
+    this.setState({ value: nextVal });
+  }
+
+  onSwipeDown() {
+    const prevVal = this.value - 1;
+    this.setState({ value: prevVal });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>{this.props.children}</Text>
-      </View>
+      <GestureRecognizer style={styles.container}
+      onSwipeUp={(state) => this.onSwipeUp()}
+      onSwipeDown={(state) => this.onSwipeDown()}>
+        <Text style={styles.label}>
+        {this.props.children}
+        </Text>
+        <Text style={styles.text} adjustsFontSizeToFit={true}>
+          {this.state.value}
+        </Text>
+      </GestureRecognizer>
     );
   }
 }
@@ -16,11 +42,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000',
     marginTop: 5,
-    marginLeft: 5,
+    marginLeft: 5
+  },
+
+  label: {
+    color: '#fff',
+    padding: 5
   },
 
   text: {
+    flex: 1,
     color: '#fff',
-    padding: 5
+    textAlign: 'center',
+    fontSize: 100
   }
 });
