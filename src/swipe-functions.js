fe2d8cc3d0076690c2ff740d07e7356a4c584362
Matt McKenna <matt@mtmckenna.com>
2018-03-03 21:19:57 -0800

    Change swipe minimums

--- src/swipe-functions.js
+++ src/swipe-functions.js
@@ -1,20 +1,21 @@
-const MIN_SWIPE_LENGTH = 6;
+const MIN_VERTICAL_SWIPE_LENGTH = 5;
+const MIN_HORIZONTAL_SWIPE_LENGTH = 15;
 
 function isSwipe(gestureState) {
-  return Math.abs(gestureState.dx) > MIN_SWIPE_LENGTH  ||
-    Math.abs(gestureState.dy) > MIN_SWIPE_LENGTH;
+  return Math.abs(gestureState.dx) > MIN_VERTICAL_SWIPE_LENGTH  ||
+    Math.abs(gestureState.dy) > MIN_HORIZONTAL_SWIPE_LENGTH;
 }
 
 function isSwipeUp(gestureState) {
-  return (isSwipe(gestureState) && gestureState.dy < MIN_SWIPE_LENGTH) ? true : false;
+  return (isSwipe(gestureState) && gestureState.dy < MIN_VERTICAL_SWIPE_LENGTH) ? true : false;
 }
 
 function isSwipeDown(gestureState) {
-  return (isSwipe(gestureState) && gestureState.dy > MIN_SWIPE_LENGTH) ? true : false;
+  return (isSwipe(gestureState) && gestureState.dy > MIN_VERTICAL_SWIPE_LENGTH) ? true : false;
 }
 
 function isSwipeHorizontal(gestureState) {
-  return (isSwipe(gestureState) && Math.abs(gestureState.dx) > MIN_SWIPE_LENGTH) ? true : false;
+  return (isSwipe(gestureState) && Math.abs(gestureState.dx) > MIN_HORIZONTAL_SWIPE_LENGTH) ? true : false;
 }
 
 export {
